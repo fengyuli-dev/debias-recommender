be8f9e06117e4248d7dc4cd75a0ffe091d7b821b
Fengyu Li <fengyuli2002@gmail.com>
2022-05-25 17:55:55 -0400

    Add method skeleton

---
+++ data_utils.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+from random import random
+import reclab
+
+
+def generate_ground_truth_matrix(dimensions, distribution='uniform', quantization_method='binary'):
+    '''
+    Generates a ground truth matrix.
+
+    Parameters:
+        dimensions: a tuple containing the dimensions of the matrix.
+        sampling_method: a string that specifies the method used to generate the ground truth.
+
+    Returns:
+        a mxn matrix containing the ground truth for each pair of m users and n items.
+    '''
+    m, n = dimensions
+
+    if distribution == 'uniform':
+        if quantization_method == 'binary':
+            return np.random.randint(2, size=dimensions)
+
+    elif distribution == 'normal':
+        if quantization_method == 'binary':
+            raise ValueError
+    pass
+
+
+def ground_truth_matrix_to_dataset(matrix, sample_prob = 0.1, bias=None, shuffle=True):
+    '''
+    Converts a ground truth matrix to a recommender dataset.
+
+    Parameters:
+        matrix: a mxn  matrix that contains the ground truth for each pair of m users and n items.
+
+    Returns:
+
+    '''
+    m, n = matrix.shape
+
+    if shuffle:
+        matrix = np.random.shuffle(matrix)
+
+    for i in range(m):
+        for j in range(n):
+            if random() > sample_prob:
+                matrix[i, j] = None
+    
+    if bias == None:
+        users = {}
+        items = {}
+        ratings = {}
+        for i in range(m):
+            users[i] = matrix[i]
+        for i in range(n):
+            items[n] = matrix[:, i]
+        for i in range(m):
+            for j in range(n):
+                ratings[(i, j)] = matrix[i, j]    
+    pass
+
+
+if __name__ == '__main__':
+    print(generate_ground_truth_matrix((10, 10)))
