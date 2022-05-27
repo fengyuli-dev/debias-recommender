2884d76a325ae0e832518fcdf11eb8b9824f5ebc
Fengyu Li <fengyuli2002@gmail.com>
2022-05-27 14:24:01 -0400

    Helper function

--- data_utils.py
+++ data_utils.py
@@ -22,44 +22,53 @@ def generate_ground_truth_matrix(dimensions, distribution='uniform', quantizatio
         if quantization_method == 'binary':
             return np.random.randint(2, size=dimensions)
 
-    elif distribution == 'normal':
-        if quantization_method == 'binary':
-            raise ValueError
-    pass
-
 
 def ground_truth_matrix_to_dataset(matrix, sample_prob=0.1, bias=None, shuffle=True):
     """
     Converts a ground truth matrix to a recommender dataset.
 
     Parameters:
-        matrix: a mxn  matrix that contains the ground truth for each pair of m users and n items.
+        matrix: a mxn matrix that contains the ground truth for each pair of m users and n items.
+        sample_prob: the probability of sampling a chosen pair of users and items.
+        bias: the type of sampling bias.
+        shuffle: whether to shuffle the dataset by users.
 
     Returns:
-
+        Same as reclab.data_utils.read_dataset()
     """
     m, n = matrix.shape
 
     if shuffle:
         np.random.shuffle(matrix)
 
-    for i in range(m):
-        for j in range(n):
-            if random() > sample_prob:
-                matrix[i, j] = None
-
     if bias is None:
-        users = {}
-        items = {}
         ratings = {}
         for i in range(m):
-            users[i] = matrix[i]
-        for i in range(n):
-            items[n] = matrix[:, i]
-        for i in range(m):
             for j in range(n):
-                ratings[(i, j)] = matrix[i, j]
-    pass
+                ratings[(i, j)] = sample(matrix[i, j], sample_prob)
+        users, items = generate_users_items(ratings, m, n)
+        return users, items, ratings
+
+
+def generate_users_items(ratings, m, n):
+    users = {}
+    items = {}
+    for i in range(m):
+        users[i] = [None] * n
+    for i in range(n):
+        items[i] = [None] * m
+
+    for (user_id, item_id), rating in ratings.items():
+        users[user_id][item_id] = rating
+        items[item_id][user_id] = rating
+    return users, items
+
+
+def sample(value, sample_prob=0.1):
+    if random() > sample_prob:
+        return None
+    else:
+        return value
 
 
 if __name__ == '__main__':
